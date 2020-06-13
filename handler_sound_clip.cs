ae6c73aecd733058cae0583a3ef4901f62586c37
TuoTuo0710 <55468288+TuoTuo0710@users.noreply.github.com>
2020-06-13 23:24:56 +0800

    Add files via upload

---
+++ handler_sound_clip.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class handler_sound_clip : MonoBehaviour
+{
+    AudioHandler aHandler;
+    [SerializeField]
+    AudioClip m_clip;
+
+    void Start()
+    {
+        aHandler = GameObject.Find("Audio_Handler").GetComponent<AudioHandler>();
+    }
+
+    void PlaySound(AudioClip aClip)
+    {
+        aHandler.PlayAudio(aClip);
+    }
+}
