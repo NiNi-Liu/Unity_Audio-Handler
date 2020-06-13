0889cfe464c09e4d2e53415e425341894748cbf2
TuoTuo0710 <55468288+TuoTuo0710@users.noreply.github.com>
2020-06-13 23:23:14 +0800

    Add files via upload

---
+++ AudioHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioHandler : MonoBehaviour
+{
+    bool canPlay = true;
+
+
+    public void PlayAudio(AudioClip clip)
+    {
+
+        if (canPlay)
+        {
+            canPlay = false;
+            GetComponent<AudioSource>().PlayOneShot(clip);
+        }
+
+        StartCoroutine(Reset());
+    }
+
+    IEnumerator Reset()
+    {
+        yield return new WaitForSeconds(.2f);
+        canPlay = true;
+    }
+}
